c1e2ef7e1add80483551a2d17cd0b0e6c1dda74d
dwleonhardt <dwleonhardt@gmail.com>
2017-08-24 11:19:06 -0600

    add daily item table and seed data

--- migrations/20170823144648_daily_item.js
+++ migrations/20170823144648_daily_item.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+  .createTable('daily_items', (table) => {
+    table.increments('id').primary().unsigned();
+    table.string('title', 225).notNullable();
+    table.string('instructions', 500);
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('daily_items');
 };
--- seeds/daily_item.js
+++ seeds/daily_item.js
@@ -1,13 +1,31 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('daily_item').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('daily_items').insert([
+        {
+          id: 1,
+          title: 'Breakfast',
+          instructions: 'Make sure he eats vegitables',
+          created_at: new Date('2016-06-26T14:26:16.000Z'),
+          updated_at: new Date ('2016-06-26T14:26:16.000Z')
+        },
+        {
+          id: 2,
+          title: 'Lunch',
+          instructions: 'He is allergic to mayo so be careful',
+          created_at: new Date('2017-06-26T14:26:16.000Z'),
+          updated_at: new Date ('2017-06-26T14:26:16.000Z')
+        },
+        {
+          id: 3,
+          title: 'Dinner',
+          instructions: 'We would like to schedule our daily phone call durring this time',
+          created_at: new Date('2014-06-26T14:26:16.000Z'),
+          updated_at: new Date ('2014-06-26T14:26:16.000Z')
+        }
       ]);
     });
 };
