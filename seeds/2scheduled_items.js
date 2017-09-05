00744d51b5d82dc858df3ed9179509a35f3d0ae2
dwleonhardt <dwleonhardt@gmail.com>
2017-09-05 11:15:23 -0600

    added extra seed data

--- seeds/2scheduled_items.js
+++ seeds/2scheduled_items.js
@@ -8,24 +8,108 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           daily_item_id: 2,
-          start_time: new Date ('2017-09-28T12:30:00.000Z'),
-          end_time: new Date ('2017-09-28T13:30:00.000Z'),
+          start_time: new Date ('2017-09-04T06:30:00.000Z'),
+          end_time: new Date ('2017-09-04T06:00:00.000Z'),
           notes: 'He did not want to eat his vegitables'
         },
         {
           id: 2,
           daily_item_id: 3,
-          start_time: new Date ('2017-09-28T17:30:00.000Z'),
-          end_time: new Date ('2017-09-28T18:30:00.000Z'),
+          start_time: new Date ('2017-09-04T12:30:00.000Z'),
+          end_time: new Date ('2017-09-04T12:40:00.000Z'),
           notes: 'Asked about Pauline',
         },
         {
           id: 3,
           daily_item_id: 1,
-          start_time: new Date ('2017-09-28T06:30:00.000Z'),
-          end_time: new Date ('2017-09-28T07:30:00.000Z'),
+          start_time: new Date ('2017-09-04T01:30:00.000Z'),
+          end_time: new Date ('2017-09-04T01:30:00.000Z'),
           notes: 'Said he would call you guys tomorrow',
-        }
+        },
+        {
+          id: 4,
+          daily_item_id: 2,
+          start_time: new Date ('2017-09-05T06:30:00.000Z'),
+          end_time: new Date ('2017-09-05T06:00:00.000Z'),
+          notes: 'He did not want to eat his vegitables'
+        },
+        {
+          id: 5,
+          daily_item_id: 3,
+          start_time: new Date ('2017-09-05T12:30:00.000Z'),
+          end_time: new Date ('2017-09-05T12:40:00.000Z'),
+          notes: 'Asked about Pauline',
+        },
+        {
+          id: 6,
+          daily_item_id: 1,
+          start_time: new Date ('2017-09-05T01:30:00.000Z'),
+          end_time: new Date ('2017-09-05T01:30:00.000Z'),
+          notes: 'Said he would call you guys tomorrow',
+        },
+        {
+          id: 7,
+          daily_item_id: 2,
+          start_time: new Date ('2017-09-06T06:30:00.000Z'),
+          end_time: new Date ('2017-09-06T06:00:00.000Z'),
+          notes: 'He did not want to eat his vegitables'
+        },
+        {
+          id: 8,
+          daily_item_id: 3,
+          start_time: new Date ('2017-09-06T12:30:00.000Z'),
+          end_time: new Date ('2017-09-06T12:40:00.000Z'),
+          notes: 'Asked about Pauline',
+        },
+        {
+          id: 9,
+          daily_item_id: 1,
+          start_time: new Date ('2017-09-06T01:30:00.000Z'),
+          end_time: new Date ('2017-09-06T01:30:00.000Z'),
+          notes: 'Said he would call you guys tomorrow',
+        },
+        {
+          id: 10,
+          daily_item_id: 2,
+          start_time: new Date ('2017-09-07T06:30:00.000Z'),
+          end_time: new Date ('2017-09-07T06:00:00.000Z'),
+          notes: 'He did not want to eat his vegitables'
+        },
+        {
+          id: 11,
+          daily_item_id: 3,
+          start_time: new Date ('2017-09-07T12:30:00.000Z'),
+          end_time: new Date ('2017-09-07T12:40:00.000Z'),
+          notes: 'Asked about Pauline',
+        },
+        {
+          id: 12,
+          daily_item_id: 1,
+          start_time: new Date ('2017-09-07T01:30:00.000Z'),
+          end_time: new Date ('2017-09-07T01:30:00.000Z'),
+          notes: 'Said he would call you guys tomorrow',
+        },
+        {
+          id: 13,
+          daily_item_id: 2,
+          start_time: new Date ('2017-09-09T06:30:00.000Z'),
+          end_time: new Date ('2017-09-09T06:00:00.000Z'),
+          notes: 'He did not want to eat his vegitables'
+        },
+        {
+          id: 14,
+          daily_item_id: 3,
+          start_time: new Date ('2017-09-09T12:30:00.000Z'),
+          end_time: new Date ('2017-09-09T12:40:00.000Z'),
+          notes: 'Asked about Pauline',
+        },
+        {
+          id: 15,
+          daily_item_id: 1,
+          start_time: new Date ('2017-09-09T01:30:00.000Z'),
+          end_time: new Date ('2017-09-09T01:30:00.000Z'),
+          notes: 'Said he would call you guys tomorrow',
+        },
       ]);
     })
     .then(() => {
