8a1901028d2dfad92e4cce9b917bead69b08b6fe
dwleonhardt <dwleonhardt@gmail.com>
2017-09-07 11:53:42 -0600

    foreign key cascade on delete

--- migrations/20170828132206_scheduled_items.js
+++ migrations/20170828132206_scheduled_items.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema
   .createTable('scheduled_items', (table) => {
     table.increments('id').primary().unsigned();
-    table.integer('daily_item_id').references('id').inTable('daily_items');
+    table.integer('daily_item_id').references('id').inTable('daily_items').onDelete('CASCADE');
     table.dateTime('start_time').notNullable();
     table.dateTime('end_time');
     table.string('notes', 500);
